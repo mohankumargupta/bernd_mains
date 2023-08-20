aaacb048f17e9440e7ba14a7fd24e38b48efa2ef
Raspberry Pi <raspberrypisig@gmail.com>
2023-08-20 14:31:13 +1000

    clean out the trash

--- build/libraries/Wire/utility/twi.c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Mohan\Developer\wowki\bernd_mains\build\libraries\Wire\utility\twi.c.o: \
- C:\Users\Mohan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.c \
- C:\Users\Mohan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/Arduino.h \
- C:\Users\Mohan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino/binary.h \
- C:\Users\Mohan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\variants\eightanaloginputs/pins_arduino.h \
- c:\users\mohan\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.6\variants\standard\pins_arduino.h \
- C:\Users\Mohan\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\libraries\Wire\src\utility\twi.h
